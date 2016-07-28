ae315bb8a3ae645fee21f74fe749d3100d5cc658
Natadu <edwin.gaunt@brainbug.io>
2016-07-28 17:30:25 +0200

    Update server.js

--- server.js
+++ server.js
@@ -117,7 +117,7 @@ handlers.getLeaderBoardStatistics = function(args)
     			"Rank",
     			"Xp"]
 	});
-	return {Stats:playerStatistics};
+	return {playerStatistics};
 }
 
 handlers.addCityBuilding =function(args)
